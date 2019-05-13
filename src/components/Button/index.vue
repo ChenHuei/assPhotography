d6cb47a0610d5a96c608adc7d4bceaf33b92b849
Huei <aaa830714@gmail.com>
2019-05-13 17:50:05 +0800

    Init project setting

---
+++ src/components/Button/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="button">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/import';
+.button {
+
+}
+</style>
